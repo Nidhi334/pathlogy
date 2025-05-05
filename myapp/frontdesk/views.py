3796da423d5382514d44163e3217b1d0e7486def
nidhi334 <knidhibharti8178@gmail.com>
2025-05-05 14:14:17 +0530

    Refactor TestGroup and Test viewsets to simplify update and destroy methods with consistent success messages

--- myapp/frontdesk/views.py
+++ myapp/frontdesk/views.py
@@ -48,19 +48,18 @@ class TestGroupViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def update(self, request, *args, **kwargs):
-        user = request.user
-        instance = self.get_object()
-        data = request.data.copy()
-        if not user.is_staff and instance.created_by.id != user.id:
-            return Response({"error": "You are not allowed to update this object."},
-                        status=status.HTTP_403_FORBIDDEN)
-        if 'created_by' in data:
-            del data['created_by']
-        serializer = self.get_serializer(instance, data=data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        super().update(request, *args, **kwargs)
+        return Response(
+            {"message": "Testgroup updated successfully"},
+            status=status.HTTP_200_OK
+        )
     
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {"message": "Testgroup deleted successfully"},
+            status=status.HTTP_200_OK
+        )
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
@@ -75,18 +74,18 @@ class TestViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def update(self, request, *args, **kwargs):
-        user = request.user
-        instance = self.get_object()
-        data = request.data.copy()
-        if not user.is_staff and instance.created_by.id != user.id:
-            return Response({"error": "You are not allowed to update this object."},
-                        status=status.HTTP_403_FORBIDDEN)
-        if 'created_by' in data:
-            del data['created_by']
-        serializer = self.get_serializer(instance, data=data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        super().update(request, *args, **kwargs)
+        return Response(
+            {"message": "Test updated succesfully"},
+            status=status.HTTP_200_OK
+        )
+    
+    def destroy (self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {"message":"Test deleted successfully"},
+            status=status.HTTP_200_OK
+        )
     
 class PackageViewSet(viewsets.ModelViewSet):
     queryset = Package.objects.all()
